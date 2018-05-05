f5ffd32bbc3dcd725978da477ae0cf632308c00e
MiracleWong <cfwr1991@126.com>
2018-05-05 17:03:03 +0800

    Step8: add the blog detail view

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.six import python_2_unicode_compatible
+from django.urls import reverse
 
 # Create your models here.
 
@@ -34,3 +35,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # # 自定义的get_absolute_url 的方法
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
-
-
-# def index(request):
-#     # return HttpResponse("欢迎访问我的博客首页！")
-#     return render(request, 'blog/index.html', context={
-#         'title': '我的博客首页',
-#         'welcome': '欢迎访问我的博客首页'
-#     })
 from .models import Post
 
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def detail(request, pk):
+    # print(request.body)
+    # return HttpResponse("欢迎访问我的博客首页！")
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', context={'post': post})
