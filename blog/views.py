1ef2b0b0dcd0e3d3ccd3002d939f587cee0579e6
MiracleWong <cfwr1991@126.com>
2018-05-06 14:47:51 +0800

    Step12: comments

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from comments.forms import CommentForm
 # Create your views here.
 from .models import Post, Category
 
@@ -20,7 +20,13 @@ def detail(request, pk):
         'markdown.extensions.codehilite',
         'markdown.extensions.toc',
     ])
-    return render(request, 'blog/detail.html', context={'post': post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
 
 
 def archives(request, year, month):
