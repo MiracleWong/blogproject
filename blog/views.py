5a4ae3490713411f008cab92ff1de4d3ba8a12f0
MiracleWong <cfwr1991@126.com>
2018-09-24 16:35:11 +0800

    Step25: simple search

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from comments.forms import CommentForm
 # Create your views here.
 from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView
-
+from django.db.models import Q
 
 class IndexView(ListView):
     model = Post
@@ -244,3 +244,15 @@ class TagView(ListView):
     def get_queryset(self):
         tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
         return super(TagView, self).get_queryset().filter(tags=tag)
+
+def search(request):
+    q = request.GET.get('q')
+    error_msg = ''
+
+    if not q:
+        error_msg = "请输入关键词"
+        return render(request, 'blog/index.html', {'error_msg': error_msg})
+
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request, 'blog/index.html', {'error_msg': error_msg,
+                                               'post_list': post_list})
